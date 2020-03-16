7e4bf4665ed93ac8546bbb7925aec3238ae4bfa3
Francesco Mancini <37000331+francesco1997@users.noreply.github.com>
2020-03-16 17:12:49 +0100

    Changed project structure

---
+++ src/mancini/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mancini;
+
+/**
+ * @author fmancini
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+
+	}
+
+}
